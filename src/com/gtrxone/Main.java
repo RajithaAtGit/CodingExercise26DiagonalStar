554199d4e01bbcf588ec02e305544d58b71a7876
Goonawardana R N W <gtrxone@gmail.com>
2021-11-21 04:58:59 +0530

    Initial commit

---
+++ src/com/gtrxone/Main.java
@@ -0,0 +1,33 @@
+package com.gtrxone;
+
+public class Main {
+    public static void main(String[] args) {
+
+        printSquareStar(100);
+    }
+
+    public static void printSquareStar(int number) {
+        if (number >= 5) {
+            for (int row = 0; row < number; row++) {
+                for (int col = 0; col < number; col++) {
+                    if (row == 0 || row == number - 1) {
+                        System.out.print("*");
+                    } else if (col == 0 || col == number - 1) {
+                        System.out.print("*");
+                    }else if (row==col){
+                        System.out.print("*");
+                    }else if (col==number-(row+1)){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.println();
+            }
+        } else
+            System.out.println("Invalid Value");
+    }
+
+
+}
